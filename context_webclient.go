259032cf52d2318368de4b07eb802857f2a46bbb
BalamuruganP25 <50692411+BalamuruganP25@users.noreply.github.com>
2019-06-16 13:56:35 +0000

    Update context_webclient.go

--- context_webclient.go
+++ context_webclient.go
@@ -10,38 +10,24 @@ import(
 )
 
 
-
-
 func main(){
-
+	
  ctx := context.Background()
  ctx, cancel := context.WithTimeout(ctx,time.Second)
  defer cancel()
  req,err := http.NewRequest(http.MethodGet,"http://192.168.0.166:8081",nil)
 
- 	if err != nil{
-
-  		log.Fatal(err)
-
- 	}
-
- 	req = req.WithContext(ctx)
-
-    res,err := http.DefaultClient.Do(req)
-
-  	if err != nil{
-
-  		log.Fatal(err)
-  	}
-
-  	defer res.Body.Close()
-
-  	if res.StatusCode != http.StatusOK{
-
-  			log.Fatal(res.Status)
-
-  	}
-
-  	io.Copy(os.Stdout,res.Body)
-
+   if err != nil{
+      log.Fatal(err)
+   }
+ req = req.WithContext(ctx)
+ res,err := http.DefaultClient.Do(req)
+   if err != nil{
+       log.Fatal(err)
+   }
+ defer res.Body.Close()
+   if res.StatusCode != http.StatusOK{
+       log.Fatal(res.Status)
+   }
+ io.Copy(os.Stdout,res.Body)
 }
