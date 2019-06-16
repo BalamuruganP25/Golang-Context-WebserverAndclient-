b88162d23b232b41cc9a330728eeaa44df0c189b
BalamuruganP25 <50692411+BalamuruganP25@users.noreply.github.com>
2019-06-16 13:52:18 +0000

    Add files via upload

---
+++ context_webserver.go
@@ -0,0 +1,33 @@
+package main
+import(
+	"fmt"
+	"log"
+	"time"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter,r *http.Request){
+
+ctx := r.Context()
+log.Printf("handler started")
+
+defer log.Printf("handler end")
+
+select {
+case <- time.After(5 * time.Second):
+	fmt.Fprintln(w,"hello")
+case <- ctx.Done():
+	err := ctx.Err()
+	log.Print(err)
+	http.Error(w,err.Error(),http.StatusInternalServerError)
+
+}
+
+}
+
+func main(){
+
+http.HandleFunc("/",handler)
+log.Fatal(http.ListenAndServe("192.168.0.166:8081",nil))
+
+}
